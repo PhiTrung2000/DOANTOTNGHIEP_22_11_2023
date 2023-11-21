c2eafcfc63f2a411c0279a257b08135bc86448a1
PhiTrungVip <trungnguyen7500@gmail.com>
2023-11-22 00:29:12 +0700

    update22_11_add

---
+++ src/main/java/com/mycompany/spring_mvc_project_final/entities/BookingEntity.java
@@ -0,0 +1,88 @@
+package com.mycompany.spring_mvc_project_final.entities;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "booking")
+public class BookingEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "accountId")
+    private AccountEntity account;
+
+    @OneToMany(mappedBy = "booking",fetch = FetchType.LAZY)
+    private List<PaymentEntity> paymentEntityList;
+
+    @OneToOne(cascade = {CascadeType.ALL})
+    @PrimaryKeyJoinColumn
+    private DiscountEntity discount;
+
+    public BookingEntity(){
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public DiscountEntity getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(DiscountEntity discount) {
+        this.discount = discount;
+    }
+
+    public AccountEntity getAccount() {
+        return account;
+    }
+
+    public void setAccount(AccountEntity account) {
+        this.account = account;
+    }
+
+    public List<PaymentEntity> getPaymentEntityList() {
+        return paymentEntityList;
+    }
+
+    public void setPaymentEntityList(List<PaymentEntity> paymentEntityList) {
+        this.paymentEntityList = paymentEntityList;
+    }
+
+}
